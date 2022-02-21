aad4747c1eadbb107c2cea24f94553ae89dc3bf2
juwild <juwild@gmail.com>
2022-02-21 11:36:27 -0300

    Classe de dominio e banco em memória H2

--- src/main/java/com/example/demo/SgcmWebApplication.java
+++ src/main/java/com/example/demo/SgcmWebApplication.java
@@ -1,13 +1,37 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SgcmWebApplication {
+import com.example.demo.dominio.Especialidade;
+import com.example.demo.dominio.Medico;
+import com.example.demo.repositorio.EspecialidadeRepositorio;
+import com.example.demo.repositorio.MedicoRepositorio;
 
+@SpringBootApplication
+public class SgcmWebApplication  implements CommandLineRunner{
+
+	@Autowired
+	private EspecialidadeRepositorio especialidadeRepositorio;
+	@Autowired
+	private MedicoRepositorio medicoRepositorio;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SgcmWebApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Especialidade especialidade = new Especialidade (null, "Cardiologia", " Cardiologista é o especialista nas doenças do coração e seu sistema circulatório");
+		
+		Medico medico = new Medico(null, 5458, "Maria Brown", "988888888", "maria@gmail.com", especialidade);
+				
+		especialidade.getMedicos().addAll(Arrays.asList(medico));
+		this.especialidadeRepositorio.saveAll(Arrays.asList(especialidade));
+		this.medicoRepositorio.saveAll(Arrays.asList(medico));
+	}
 }
---
+++ src/main/java/com/example/demo/dominio/Medico.java
@@ -0,0 +1,108 @@
+package com.example.demo.dominio;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Medico implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer crm;
+	private String nome;
+	private String telefone;
+	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "especialidade_id")
+	private Especialidade especialidade;
+
+	public Medico() {
+		super();		
+	}	
+
+	public Medico(Integer id, Integer crm, String nome, String telefone, String email, Especialidade especialidade) {
+		super();
+		this.id = id;
+		this.crm = crm;
+		this.nome = nome;
+		this.telefone = telefone;
+		this.email = email;
+		this.especialidade = especialidade;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCrm() {
+		return crm;
+	}
+
+	public void setCrm(Integer crm) {
+		this.crm = crm;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Medico other = (Medico) obj;
+		return Objects.equals(id, other.id);
+	}
+}
