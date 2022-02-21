5fa63afa292ae18dbd06241ff23b1fdbd94b3bdc
juwild <juwild@gmail.com>
2022-02-21 09:48:07 -0300

    Criando projeto

---
+++ src/main/java/com/example/demo/SgcmWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcmWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcmWebApplication.class, args);
+	}
+
+}
